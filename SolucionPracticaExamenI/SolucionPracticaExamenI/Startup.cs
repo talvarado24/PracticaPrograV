b112663c06637b60a4c7bd1f0b1e8d7c51079261
Laboratorios <Laboratorios@V18>
2018-10-20 13:16:48 -0600

    Se agregó Solución y proyecto WCF

---
+++ SolucionPracticaExamenI/SolucionPracticaExamenI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SolucionPracticaExamenI.Startup))]
+namespace SolucionPracticaExamenI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
